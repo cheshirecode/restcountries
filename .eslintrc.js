bd642e39e5b1af53af34c5e754eaeb68f05a3580
fred <dac4158+github@gmail.com>
2021-09-14 22:12:28 +0800

    feat: add eslint + prettier

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 10,
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    browser: true,
+    jest: true,
+  },
+  plugins: ['prettier', 'react', '@typescript-eslint', 'jsx-a11y'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'react-app',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    'no-unreachable': 'error',
+    'no-console': 'error',
+  },
+};
