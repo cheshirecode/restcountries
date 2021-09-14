678a93c11d75740a88d5dced6143dfa3bdb5ed1f
cheshireCode <1631630+cheshirecode@users.noreply.github.com>
2021-09-14 23:36:12 +0800

    feat: add dark/light theme files + web fonts

--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -15,6 +15,7 @@ export default {
         ...(process.versions.pnp ? { tsc: 'yarn pnpify tsc' } : {}),
       },
     ],
+    'snowpack-plugin-relative-css-urls',
     '@snowpack/plugin-postcss',
   ],
   routes: [
