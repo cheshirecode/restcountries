289eb08038932fbc7352dc817670a4c89d888026
fred <dac4158+github@gmail.com>
2021-09-14 22:56:20 +0800

    feat: add postcss

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+module.exports = {
+  plugins: [
+    // Replace below with your plugins
+    require('cssnano'),
+    require('postcss-preset-env'),
+  ],
+};
