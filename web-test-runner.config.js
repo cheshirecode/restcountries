6e6dbaff42e6b94d78922d70b24c30e7d9bb3c31
cheshireCode <1631630+cheshirecode@users.noreply.github.com>
2021-09-15 16:54:37 +0800

    chore: test coverage ignore

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -1,5 +1,8 @@
 process.env.NODE_ENV = 'test';
 
 module.exports = {
+  coverageConfig: {
+    exclude: ['**/*/_snowpack/**/*', '**/*/*.config.*', '**/styles/**/*.css'],
+  },
   plugins: [require('@snowpack/web-test-runner-plugin')()],
 };
