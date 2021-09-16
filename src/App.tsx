7cbd838fca174ba25c58aba10c308c25202e5164
fred <dac4158+github@gmail.com>
2021-09-16 16:39:25 +0800

    feat: flex container layout

--- src/App.tsx
+++ src/App.tsx
@@ -5,30 +5,39 @@ import ThemeHandler from './components/ThemeHandler';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
-import { Box } from 'theme-ui';
+import { Flex, Box } from 'theme-ui';
 import type { BaseComponent } from './typings';
 import { cx } from '@emotion/css';
 import styled from '@emotion/styled';
 
 const App: FC<BaseComponent> = ({ className }) => {
   return (
-    <Box
+    <Flex
+      // additional class name for escape hatch if external styling is required outside the code
       className={cx(className, 'layout')}
       sx={{
         // root-level font size for typographic scale
         fontSize: [1, 2],
+        height: '100vh',
+        flexDirection: 'column',
       }}
     >
-      <Box>
+      <Box py={[1, 2]}>
         <Header data-testid="header" className="layout--header" />
       </Box>
-      <Box>
+      <Box
+        sx={{
+          border: 'thin',
+          borderColor: 'text', // currently --theme-ui-colors-text doesn't work yet
+          flex: '1 1 auto',
+        }}
+      >
         <Main data-testid="main" className="layout--main" />
       </Box>
-      <Box>
+      <Box py={[1, 2]}>
         <Footer data-testid="footer" className="layout--main" />
       </Box>
-    </Box>
+    </Flex>
   );
 };
 
