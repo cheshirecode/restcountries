ee96c6e5abe031903c2bd9fcc8090f1b6008fb4c
cheshireCode <1631630+cheshirecode@users.noreply.github.com>
2021-09-17 21:59:35 +0800

    chore: enable eslint-plugin-import

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,8 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
     'prettier',
     'react-app',
   ],
