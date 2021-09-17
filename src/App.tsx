6367d47b05da3298d4c4574f98c06c50bbb3dae7
cheshireCode <1631630+cheshirecode@users.noreply.github.com>
2021-09-17 21:52:54 +0800

    refactor: remove ThemeHandler as it has no purpose

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,49 @@
 /** @jsx jsx */
 import type { FC } from 'react';
 import { jsx } from '@emotion/react';
-import ThemeHandler from './components/ThemeHandler';
+import { ThemeProvider, Flex, Box } from 'theme-ui';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
-import { Flex, Box } from 'theme-ui';
 import type { BaseComponent } from './typings';
 import { cx } from '@emotion/css';
+import theme from './styles/theme';
 
 const App: FC<BaseComponent> = ({ className }) => {
   return (
-    <Flex
-      // additional class name for escape hatch if external styling is required outside the code
-      className={cx(className, 'layout')}
-      variant="layout.root"
-      sx={{
-        height: '100vh',
-        flexDirection: 'column',
-        backgroundColor: 'background',
-        color: 'text',
-      }}
-      data-testid="root"
-    >
-      <Box bg="muted" as="header" data-testid="header" variant="layout.header">
-        <Header className="layout--header" />
-      </Box>
-      <Box
-        as="main"
-        data-testid="main"
-        variant="layout.mainDebug" // TODO - use layout.main after working version
+    <ThemeProvider theme={theme}>
+      <Flex
+        // additional class name for escape hatch if external styling is required outside the code
+        className={cx(className, 'layout')}
+        variant="layout.root"
         sx={{
-          flex: '1 1 auto',
-          py: 0,
+          height: '100vh',
+          flexDirection: 'column',
+          backgroundColor: 'background',
+          color: 'text',
         }}
+        data-testid="root"
       >
-        <Main className="layout--main" />
-      </Box>
-      <Box as="footer" variant="layout.footer">
-        <Footer data-testid="footer" className="layout--footer" />
-      </Box>
-    </Flex>
+        <Box bg="muted" as="header" data-testid="header" variant="layout.header">
+          <Header className="layout--header" />
+        </Box>
+        <Box
+          as="main"
+          data-testid="main"
+          variant="layout.mainDebug" // TODO - use layout.main after working version
+          sx={{
+            flex: '1 1 auto',
+            py: 0,
+          }}
+        >
+          <Main className="layout--main" />
+        </Box>
+        <Box as="footer" variant="layout.footer">
+          <Footer data-testid="footer" className="layout--footer" />
+        </Box>
+      </Flex>
+    </ThemeProvider>
   );
 };
 
-const ThemedApp: FC = ({ children, ...props }) => {
-  return (
-    <ThemeHandler {...props}>
-      <App />
-    </ThemeHandler>
-  );
-};
-
-export default ThemedApp;
+export default App;
