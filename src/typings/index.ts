24e30fef28ce43b794ca4e965d515cd7e8e327fd
cheshireCode <1631630+cheshirecode@users.noreply.github.com>
2021-09-18 22:31:13 +0800

    docs: typing - Country

--- src/typings/index.ts
+++ src/typings/index.ts
@@ -5,3 +5,68 @@ export interface BaseComponent {
   className?: string;
   ['data-testid']?: string;
 }
+
+export interface ErrorHttp extends Error {
+  response?: unknown;
+}
+
+// generated from https://app.quicktype.io/
+export interface Country {
+  name: string;
+  topLevelDomain: string[];
+  alpha2Code: string;
+  alpha3Code: string;
+  callingCodes: string[];
+  capital: string;
+  altSpellings: string[];
+  region: string;
+  subregion: string;
+  population: number;
+  latlng: number[];
+  demonym: string;
+  area: number;
+  gini: number;
+  timezones: string[];
+  borders: string[];
+  nativeName: string;
+  numericCode: string;
+  currencies: Currency[];
+  languages: Language[];
+  translations: Translations;
+  flag: string;
+  regionalBlocs: RegionalBloc[];
+  cioc: string;
+}
+
+export interface Currency {
+  code: string;
+  name: string;
+  symbol: string;
+}
+
+export interface Language {
+  iso639_1: string;
+  iso639_2: string;
+  name: string;
+  nativeName: string;
+}
+
+export interface RegionalBloc {
+  acronym: string;
+  name: string;
+  otherAcronyms: string[];
+  otherNames: string[];
+}
+
+export interface Translations {
+  de: string;
+  es: string;
+  fr: string;
+  ja: string;
+  it: string;
+  br: string;
+  pt: string;
+  nl: string;
+  hr: string;
+  fa: string;
+}
