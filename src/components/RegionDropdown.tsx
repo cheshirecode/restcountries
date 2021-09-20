163fcbe1ddf415e645be8e97a68af55354aa6361
fred <dac4158+github@gmail.com>
2021-09-20 14:21:37 +0800

    feat: style react-select with className prefixes

--- src/components/RegionDropdown.tsx
+++ src/components/RegionDropdown.tsx
@@ -1,7 +1,9 @@
 /** @jsx jsx */
 import { useMemo } from 'react';
 import type { FC } from 'react';
-import { jsx, Select, Alert } from 'theme-ui';
+import { jsx, Alert } from 'theme-ui';
+import Select from 'react-select';
+import type { CSSObject } from '@emotion/serialize';
 import useCountryListFetch from '../hooks/useCountryListFetch';
 import ApiResponseHandler from './ApiResponseHandler';
 
@@ -12,20 +14,43 @@ const RegionDropdown: FC<{ className?: string }> = ({ className }) => {
   // take all regions from country list and turn them into a list
   const regions = useMemo(() => [...new Set(data?.map((d) => d.region).filter((x) => x))], [data]);
   regions.sort();
-  const regionOptions: { id: string; displayName: string }[] = regions.map((x) => ({
+  const options: { id: string; value: string; label: string }[] = regions.map((x) => ({
     id: x,
-    displayName: x,
+    value: x,
+    label: x,
   }));
+  const classNamePrefix = 'react-select';
+
   return (
     <ApiResponseHandler data={data} error={error} data-testid="country-list">
       {
         // show nothing if the list is empty
-        regionOptions.length ? (
-          <Select defaultValue={regionOptions[0]?.displayName} className={className}>
-            {regionOptions.map(({ id, displayName }) => (
-              <option key={id}>{displayName}</option>
-            ))}
-          </Select>
+        options.length ? (
+          <Select
+            className={className}
+            defaultValue={options[0]}
+            options={options}
+            // escape hatch to apply style to children from outside through classNames with set prefixes https://react-select.com/styles#using-classnames
+            classNamePrefix={classNamePrefix}
+            sx={{
+              [`.${classNamePrefix}__indicator-separator`]: {
+                visibility: 'hidden',
+              },
+              [`.${classNamePrefix}__control`]: {
+                color: 'text',
+              },
+              [`.${classNamePrefix}__menu`]: {
+                color: 'background',
+              },
+            }}
+            // due to CSS specificity, the inner option styling needs to be reset
+            styles={{
+              option: (base: CSSObject) => ({
+                ...base,
+                color: 'inherit',
+              }),
+            }}
+          />
         ) : (
           <Alert role="alert">No regions</Alert>
         )
