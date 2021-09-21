6fe426f51dd9e1a51674b0629eadab006d1012bd
cheshireCode <1631630+cheshirecode@users.noreply.github.com>
2021-09-21 23:59:21 +0800

    refactor: fetches with good defaults

--- src/hooks/useCountryListFetch.ts
+++ src/hooks/useCountryListFetch.ts
@@ -2,15 +2,16 @@
 import useFetch from '../hooks/useFetch';
 import endpoints from '../utils/endpoints';
 import type { Country, ErrorHttp, FetchResponse } from '../typings';
+import consts from '../utils/const';
 
 export interface CountryListFetchParams {
   fields?: string[];
 }
 /** fetches all countries with minimal fields based on what country list design is, can override from outside  */
-const useCountryListFetch: (params?: CountryListFetchParams) => FetchResponse<Country[], ErrorHttp> = (
-  params = { fields: ['flag', 'name', 'population', 'region', 'capital'] },
-) => {
-  const { data, error } = useFetch<Country[]>(endpoints.getAllCountries(params?.fields));
+const useCountryListFetch: (params?: CountryListFetchParams) => FetchResponse<Country[], ErrorHttp> = (params) => {
+  const { data, error } = useFetch<Country[]>(
+    endpoints.getAllCountries(params?.fields ?? consts.COMMON_COUNTRY_FIELDS),
+  );
   data?.sort((a, b) => a.name?.localeCompare(b.name));
   return [data, error];
 };
