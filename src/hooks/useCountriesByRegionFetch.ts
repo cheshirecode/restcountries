f48895d00deec49367cc8a161a14803ea6d0f267
fred <dac4158+github@gmail.com>
2021-09-21 23:59:21 +0800

    refactor: fetches with good defaults

--- src/hooks/useCountriesByRegionFetch.ts
+++ src/hooks/useCountriesByRegionFetch.ts
@@ -2,15 +2,18 @@
 import useFetch from '../hooks/useFetch';
 import endpoints from '../utils/endpoints';
 import type { Country, ErrorHttp, FetchResponse } from '../typings';
-export interface CountryByRegionFetchParams {
+import consts from '../utils/const';
+export interface CountriesByRegionFetchParams {
   fields?: string[];
   region: string;
 }
 
-const useCountriesByRegionFetch: (params: CountryByRegionFetchParams) => FetchResponse<Country[], ErrorHttp> = (
+const useCountriesByRegionFetch: (params: CountriesByRegionFetchParams) => FetchResponse<Country[], ErrorHttp> = (
   params,
 ) => {
-  const { data, error } = useFetch<Country[]>(endpoints.getCountriesByRegion(params.region));
+  const { data, error } = useFetch<Country[]>(
+    endpoints.getCountriesByRegion(params.region, params?.fields ?? consts.COMMON_COUNTRY_FIELDS),
+  );
 
   return [data, error];
 };
