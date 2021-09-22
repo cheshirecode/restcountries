6a73e20f923cffc6e7a21a0e03b705d09bd30a65
fred <dac4158+github@gmail.com>
2021-09-22 11:15:43 +0800

    feat: fallback to mocked responses due to CORS error from service

--- src/hooks/useCountriesByNameFetch.ts
+++ src/hooks/useCountriesByNameFetch.ts
@@ -3,6 +3,7 @@ import useFetch from '../hooks/useFetch';
 import endpoints from '../utils/endpoints';
 import type { Country, ErrorHttp, FetchResponse } from '../typings';
 import consts from '../utils/const';
+import countries from '../data/countries.json';
 export interface CountriesByNameFetchParams {
   fields?: string[];
   name: string;
@@ -11,9 +12,16 @@ export interface CountriesByNameFetchParams {
 const useCountriesByNameFetch: (params: CountriesByNameFetchParams) => FetchResponse<Country[], ErrorHttp> = (
   params,
 ) => {
-  const { data, error } = useFetch<Country[]>(
+  let { data, error } = useFetch<Country[]>(
     endpoints.getCountriesByName(params.name, params?.fields ?? consts.COMMON_COUNTRY_FIELDS),
   );
+  // TODO - workaround for broken API
+  if (!data) {
+    data = (countries as Country[]).filter((x) =>
+      x.name?.toLocaleLowerCase().includes(params?.name.toLocaleLowerCase()),
+    );
+    error = undefined;
+  }
 
   return [data, error];
 };
--- src/hooks/useCountriesByRegionFetch.ts
+++ src/hooks/useCountriesByRegionFetch.ts
@@ -3,6 +3,7 @@ import useFetch from '../hooks/useFetch';
 import endpoints from '../utils/endpoints';
 import type { Country, ErrorHttp, FetchResponse } from '../typings';
 import consts from '../utils/const';
+import countries from '../data/countries.json';
 export interface CountriesByRegionFetchParams {
   fields?: string[];
   region: string;
@@ -11,11 +12,16 @@ export interface CountriesByRegionFetchParams {
 const useCountriesByRegionFetch: (params: CountriesByRegionFetchParams) => FetchResponse<Country[], ErrorHttp> = (
   params,
 ) => {
-  const { data, error } = useFetch<Country[]>(
+  let { data, error } = useFetch<Country[]>(
     params?.region
       ? endpoints.getCountriesByRegion(params.region, params?.fields ?? consts.COMMON_COUNTRY_FIELDS)
       : null,
   );
+  // TODO - workaround for broken API
+  if (!data && params?.region) {
+    data = (countries as Country[]).filter((x) => x.continent === params?.region);
+    error = undefined;
+  }
 
   return [data, error];
 };
