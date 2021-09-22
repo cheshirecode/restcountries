e3f1943f8bf983876d085a00a7028d5d67c2c843
fred <dac4158+github@gmail.com>
2021-09-22 19:12:13 +0800

    perf: allow 1 retry since API is broken, no need too many

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import type { FC } from 'react';
 import { jsx, ThemeProvider, Flex, Box } from 'theme-ui';
 import ErrorBoundary from './components/ErrorBoundary';
+import { SWRConfig } from 'swr';
 import Header from './components/Header';
 import Routes from './pages';
 import type { BaseComponent } from './typings';
@@ -10,36 +11,38 @@ import theme from './styles/theme';
 
 const App: FC<BaseComponent> = ({ className }) => {
   return (
-    <ThemeProvider theme={theme}>
-      <Flex
-        // additional class name for escape hatch if external styling is required outside the code
-        className={cx(className, 'layout')}
-        variant="layout.root"
-        sx={{
-          height: '100vh',
-          flexDirection: 'column',
-          backgroundColor: 'background',
-          color: 'text',
-        }}
-        data-testid="root"
-      >
-        <Box bg="muted" as="header" data-testid="header" variant="layout.header">
-          <ErrorBoundary>
-            <Header className="layout--header" />
-          </ErrorBoundary>
-        </Box>
-        <Box
-          as="main"
-          data-testid="main"
-          variant="layout.mainDebug" // TODO - use layout.main after working version
+    <SWRConfig value={{ errorRetryCount: 1 }}>
+      <ThemeProvider theme={theme}>
+        <Flex
+          // additional class name for escape hatch if external styling is required outside the code
+          className={cx(className, 'layout')}
+          variant="layout.root"
+          sx={{
+            height: '100vh',
+            flexDirection: 'column',
+            backgroundColor: 'background',
+            color: 'text',
+          }}
+          data-testid="root"
         >
-          <ErrorBoundary>
-            <Routes />
-          </ErrorBoundary>
-        </Box>
-        <Box as="footer" variant="layout.footer"></Box>
-      </Flex>
-    </ThemeProvider>
+          <Box bg="muted" as="header" data-testid="header" variant="layout.header">
+            <ErrorBoundary>
+              <Header className="layout--header" />
+            </ErrorBoundary>
+          </Box>
+          <Box
+            as="main"
+            data-testid="main"
+            variant="layout.mainDebug" // TODO - use layout.main after working version
+          >
+            <ErrorBoundary>
+              <Routes />
+            </ErrorBoundary>
+          </Box>
+          <Box as="footer" variant="layout.footer"></Box>
+        </Flex>
+      </ThemeProvider>
+    </SWRConfig>
   );
 };
 
