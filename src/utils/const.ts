05e5377f014e5be3e21edb507a71922f5cfa81ed
fred <dac4158+github@gmail.com>
2021-09-22 11:13:33 +0800

    feat: use https://restcountries.com instead of https://restcountries.eu

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,11 +1,25 @@
+// broke as of 2021-09-22
+// const consts = {
+//   API_BASE_URL: 'https://restcountries.eu',
+//   API_ENDPOINTS: {
+//     GET_ALL_COUNTRIES: 'https://restcountries.eu/rest/v2/all',
+//     GET_COUNTRIES_BY_REGION: '/rest/v2/region',
+//     GET_COUNTRIES_BY_NAME: '/rest/v2/name',
+//   },
+//   COMMON_COUNTRY_FIELDS: ['flag', 'name', 'population', 'region', 'capital'],
+//   FIELDS_SEPARATOR: ';',
+// };
+
+// using https://restcountries.com instead
 const consts = {
-  API_BASE_URL: 'https://restcountries.eu',
+  API_BASE_URL: 'https://restcountries.com',
   API_ENDPOINTS: {
-    GET_ALL_COUNTRIES: 'https://restcountries.eu/rest/v2/all',
-    GET_COUNTRIES_BY_REGION: '/rest/v2/region',
-    GET_COUNTRIES_BY_NAME: '/rest/v2/name',
+    GET_ALL_COUNTRIES: '/v2/all',
+    GET_COUNTRIES_BY_REGION: '/v2/continent',
+    GET_COUNTRIES_BY_NAME: '/v2/name',
   },
-  COMMON_COUNTRY_FIELDS: ['flag', 'name', 'population', 'region', 'capital'],
+  COMMON_COUNTRY_FIELDS: ['flag', 'name', 'population', 'region', 'capital', 'flags', 'continent'],
+  FIELDS_SEPARATOR: ',',
 };
 
 export default consts;
